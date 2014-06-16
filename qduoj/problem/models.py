450eb7b56f892caff8d185feab273f0d5fbd4d01
zlx <luxvxul@gmail.com>
2014-06-16 21:06:42 +0800

    problem model modify

--- qduoj/problem/models.py
+++ qduoj/problem/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from oj_user.models import User
+from oj_user.models import User
 
 class Problem(models.Model):
     title = models.CharField(max_length = 200, default = '')
@@ -8,7 +8,7 @@ class Problem(models.Model):
     pro_output = models.TextField()
     sample_input = models.TextField()
     sample_output = models.TextField()
-    spj = models.CharField(max_length = 1, default = '0') #max_length
+    spj = models.CharField(max_length = 1, default = '0') 
     hint = models.TextField()
     source = models.CharField(max_length = 100, null = True)
     in_date = models.DateTimeField(null = True)
@@ -18,4 +18,8 @@ class Problem(models.Model):
     accepted = models.IntegerField(default = 0)
     submit = models.IntegerField(default = 0)
     solved = models.IntegerField(default = 0)
-  # user = models.ForeignKey(User)
+    user = models.ForeignKey(User)
+
+class Score(models.Model):
+    problem_id = models.ForeignKey(Problem, primary_key = True)
+    score = models.IntegerField(default = 0)    
