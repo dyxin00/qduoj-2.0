698afb258603609f031bc81823406df93fc4b083
dyxin00 <dyxin00@gmail.com>
2014-06-22 10:44:49 +0800

    problem test

--- qduoj/problem/views.py
+++ qduoj/problem/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+
+    return render(request,"problem/problem_list.html", {})
+def problem(request):
+
+    return render(request,"problem/problem.html", {})
