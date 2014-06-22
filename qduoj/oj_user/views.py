8308b44eb18eea6073b1c5c280947d1ab31d0ee0
dyxin00 <dyxin00@gmail.com>
2014-06-22 13:25:07 +0800

    add captcha

--- qduoj/oj_user/views.py
+++ qduoj/oj_user/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from DjangoVerifyCode import Code
 
 def sign_up(request):
     return render(request, "user/sign_up.html", {})
 
 def sign_in(request):
-    return render(request, "user/sign_in.html", {})
+    return render(request, "user/sign_in.html",{})
+
+#http://www.oschina.net/p/django-verify-code
+def get_code(request):
+    code = Code(request)
+    code.img_height = 46
+    font_size = 35
+    return code.display()
+
