01ce5a0b1d0247a699f93ae3a0d663fa1a994748
dyxin00 <dyxin00@gmail.com>
2014-06-22 10:43:34 +0800

    comtest model modify

--- qduoj/contest/models.py
+++ qduoj/contest/models.py
@@ -1,23 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from oj_user.models import User
 from problem.models import Problem
-from solution.models import Solution
 
 class Contest(models.Model):
-    title = models.CharField(max_length = 255, null =True)
-    start_time = models.DateTimeField(null =True)
-    end_time = models.DateTimeField(null = True)
-    defunct = models.BooleanField(default = False)
+    title = models.CharField(max_length=255, null=True)
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+    defunct = models.BooleanField(default=False)
     description = models.TextField()
-    private = models.IntegerField(default = 0)
-    langmask = models.IntegerField(default = 0)
+    private = models.IntegerField(default=0)
+    visible = models.BooleanField(default=False)
+    langmask = models.IntegerField(default=0)
+    #mode 0 -> acm, 1->OI
+    mode = models.IntegerField(default=0)
     user = models.ForeignKey(User)
 
 class Contest_problem(models.Model):
     problem_id = models.ForeignKey(Problem)
-    title = models.CharField(max_length = 200, default = '')
-    num = models.IntegerField(default = 0)
+    title = models.CharField(max_length=200, default='')
+    num = models.IntegerField(default=0)
+    sorce = models.IntegerField()
 
-class Runtimeinfo(models.Model):
-    solution_id = models.ForeignKey(Solution)
-    error = models.TextField()
