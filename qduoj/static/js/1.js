70b4443be74d44caf2a1f4840189f9fc27931b82
dyxin00 <dyxin00@gmail.com>
2014-06-24 15:50:52 +0800

    add template

--- qduoj/static/js/1.js
+++ qduoj/static/js/1.js
@@ -2,9 +2,3 @@ junction refresh_captcha(obj) {
     obj.src = "/get_code/";
 }
 
-function display(obj){
-    document.getElementById('only').style.color='red'
-}
-$(function() {
-    $("#ee").tooltip({})
-})
