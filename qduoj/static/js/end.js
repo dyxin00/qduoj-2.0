3e325c30c1d31340f48b6b94ac356a16b632e0b3
dyxin00 <dyxin00@gmail.com>
2014-06-24 11:46:30 +0800

    submit code

---
+++ qduoj/static/js/end.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('#sign_up').tooltip({
+        selector: "[data-toggle=tooltip]",
+        container: "body"
+    })
+    $("#sign_up_id").tooltip({})
+    $("#sign_up_username").tooltip({})
+})
+
