bfee30e00273c81ec1906c25f79c0261950a9d3a
dyxin00 <dyxin00@gmail.com>
2014-06-24 15:50:05 +0800

    add funcation submut_code

--- qduoj/problem/views.py
+++ qduoj/problem/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-    
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 
     return render(request,"problem/problem_list.html", {})
+
 def problem(request):
 
     return render(request,"problem/problem.html", {})
+
+
+def submit_code(request):
+
+    if request.method == 'POST':
+
+        code = request.POST.get('code', '')
+        language = request.POST.get('language', '')
+        pid = request.POST.get('pid', '')
+        # 未验证数据正确性
+
+        return redirect('status_list')
+
+    return redirect('index')
