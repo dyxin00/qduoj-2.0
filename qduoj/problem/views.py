5bc148322f3a24a4f9b557cf7f9f9bac3bdb296d
feifei <821388334@qq.com>
2014-06-26 18:14:33 +0800

    problem_list

--- qduoj/problem/views.py
+++ qduoj/problem/views.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-    
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from problem.models import Problem
 
 def index(request):
+    if request.method == 'GET':
 
-    return render(request,"problem/problem_list.html", {})
+        problem_type = request.GET.get('type', '-1')
+
+        if problem_type == '-1':
+            problems = Problem.objects.all()
+
+        else:
+            problems = Problem.objects.filter(classify=problem_type)
+
+        return render(request, "problem/problem_list.html",
+                {'problems': problems, 'type' : problem_type})
+    
 
 def problem(request):
-
     return render(request,"problem/problem.html", {})
+
 
 @login_required(login_url='sign_in')
 def submit_code(request):
