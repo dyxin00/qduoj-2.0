5fba163795f9ddadf456a6f93223f6d3484c6040
luxv <luxvxul@gmail.com>
2014-06-30 17:23:51 +0800

    solutin list

--- qduoj/solution/views.py
+++ qduoj/solution/views.py
@@ -1,9 +1,10 @@
+# encoding:utf-8 
 from django.shortcuts import render
 from solution.models import Solution
 
 def solution_list(request):
     if request.method=="GET":
-        judge_list = Solution.objects.all().order_by('-in_date')
+        judge_list = Solution.objects.all().order_by('-id')
         return render(request, "solution/status.html", 
             {'judge_list': judge_list})
     return render(request, "solution/status.html", {})
