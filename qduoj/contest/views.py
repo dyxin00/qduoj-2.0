50bb4b198d08c48d28ebd3cccbe4184bd0f2f85c
feifei <821388334@qq.com>
2014-06-30 15:00:37 +0800

    contest list start

--- qduoj/contest/views.py
+++ qduoj/contest/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from problem.models import Problem
+from contest.models import Contest
 
 def contest_list(request):
+    contests = Contest.objects.filter(visible=False)
+    return render(request, 'contest/contest_list.html', {'contests': contests})
 
-    return render(request, 'contest/contest_list.html', {})
