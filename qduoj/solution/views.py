80f3b84b1721b452a2fbb741825267246fcaf67d
luxv <luxvxul@gmail.com>
2014-06-30 13:46:58 +0800

    status list

--- qduoj/solution/views.py
+++ qduoj/solution/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from solution.models import Solution
 
 def solution_list(request):
+    if request.method=="GET":
+        judge_list = Solution.objects.all().order_by('-in_date')
+        return render(request, "solution/status.html", 
+            {'judge_list': judge_list})
     return render(request, "solution/status.html", {})
