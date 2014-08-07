2d622abdfbb895280d8b1aefc16e2b186818970e
feifei <821388334@qq.com>
2014-08-07 17:21:19 +0800

    count

--- qduoj/oj_user/views.py
+++ qduoj/oj_user/views.py
@@ -16,6 +16,7 @@ from oj_user.models import User_oj
 from problem.models import Problem
 from solution.models import Solution
 
+from django.db.models import Sum
 
 def index(request):
     return render(request, "index.html", {})
@@ -50,7 +51,7 @@ def sign_up(request):
             error = 'This user name already exists !'
             return render(request, "user/sign_up.html", {'error' : error})
         
-        oj_user = User_oj.objects.create(user=user, school_id=school_id, accesstime = '1997-01-01')
+        oj_user = User_oj.objects.create(user=user, school_id=school_id)
         return render(request, 'delay_jump.html', {
             'next_url' : '/sign_in/',
             'info' : 'Registration successful'
@@ -103,23 +104,32 @@ def sign_out(request):
 def check_in(request):
     if request.method == 'POST':
         next_url = request.POST.get('next', '/')
-        k = request.get_full_path()
-        print k, 'dsdsd'
         user_id = request.user.id
         user_oj = User_oj.objects.get(user__id = user_id)
 
         GMT_FORMAT = '%Y-%m-%d %H:%M:%S'
         now = time.localtime()
-        t1 = user_oj.accesstime.replace() + datetime.timedelta(hours=8)
-        st = time.mktime(time.strptime( str(t1),'%Y-%m-%d'))
-        accesstime = time.localtime(st)
-        if now[0] == accesstime[0] and now[1] == accesstime[1] and now[2] == accesstime[2]:
-            return render(request, 'delay_jump.html',{
-                'next_url' : '/',
-                'info' : 'Dont recheck_in'
-                })
+        accesstime = user_oj.accesstime
+        if accesstime != None:
+            t1 = user_oj.accesstime.replace() + datetime.timedelta(hours=8)
+            st = time.mktime(time.strptime( str(t1),'%Y-%m-%d'))
+            accesstime = time.localtime(st)
+            if now[0] == accesstime[0] and now[1] == accesstime[1] and now[2] == accesstime[2]:
+                return render(request, 'delay_jump.html',{
+                    'next_url' : '/',
+                    'info' : 'Dont recheck_in'
+                    })
+            else:
+	        user_oj.integral = user_oj.integral + 1
+                user_oj.accesstime = datetime.datetime.now()
+                user_oj.save()
+            
+                return render(request, 'delay_jump.html', {
+                    'next_url' : '/',
+                    'info' : 'Check_in successful',
+                    })
         else:
-	    user_oj.integral = user_oj.integral + 1
+            user_oj.integral = user_oj.integral + 1
             user_oj.accesstime = datetime.datetime.now()
             user_oj.save()
             
@@ -127,6 +137,8 @@ def check_in(request):
                 'next_url' : '/',
                 'info' : 'Check_in successful',
                 })
+
+
 
 def user_info(request):
     if request.method=="GET":
@@ -139,20 +151,26 @@ def user_info(request):
             user_info = request.user
         solution_list = Solution.objects.filter(user_id=user_id)
     
-        accepted_list = solution_list.filter(result=4).order_by('problem').values_list('problem', flat=True).distinct()
-        unsolved_list = solution_list.exclude(result=4).order_by('problem').values_list('problem', flat=True).distinct()
+        accepted_list = solution_list.filter(result=4).order_by('problem').\
+                values_list('problem', flat=True).distinct()
+        unsolved_list = solution_list.exclude(result=4).order_by('problem').\
+                values_list('problem', flat=True).distinct()
         unsolved_num = len(list(set(unsolved_list).difference(set(accepted_list))))
-        
+        '''
         count = 0
         for ac_id in accepted_list:
             problem = Problem.objects.get(id = ac_id)
             count = count + problem.difficult
-        user_info.user_oj.integral = user_info.user_oj.integral + count
-        accesstime = user_info.user_oj.accesstime.replace() + datetime.timedelta(hours=8)
-        if str(accesstime) == "1997-01-01":
-            flag = 1
-        else:
+        '''
+        ac_problem = Problem.objects.filter(id__in = accepted_list)
+        count = ac_problem.aggregate(Sum('difficult'))
+        user_info.user_oj.integral = user_info.user_oj.integral + count['difficult__sum']
+        accesstime_info = user_info.user_oj.accesstime
+        if accesstime_info != None:
+            accesstime = accesstime_info.replace() + datetime.timedelta(hours=8)
             flag = 0
+        else:
+            flag = 1
         user_infos = {'user_info': user_info, 
                 'accepted_list': accepted_list,
                 'unsolved_list': unsolved_list,
